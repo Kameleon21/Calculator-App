489c52a4a9edf727576a4474f7ef63df9487006d
Kamil <kamilrogozinski29@gmail.com>
2023-01-10 10:03:21 +0000

    added functions for the basic operators and now working on calculator login

---
+++ script.js
@@ -0,0 +1,38 @@
+// add,multiply,divide and subtract functions
+
+function add(...args){
+    return [...args].reduce((acc,cur) => acc + cur,0);
+}
+
+function subtract(...args){
+    return [...args].reduce((acc,cur) => cur -acc,0);
+}
+
+function multiply(...args){
+    return [...args].reduce((acc,cur) => acc * cur,);
+}
+
+function divide(...args){
+    return [...args].reduce((acc,cur) => acc/cur,);
+}
+
+// calculator logic 
+let tell = prompt("What would you like to do add/subtract/divide or multiply");
+let dig1 = prompt('digt1');
+let dig2 = prompt('digt2');
+const operate = function(a,b) {
+    switch(tell) {
+        case 'add':
+            return add(a,b);
+        case 'subtract':
+            return subtract(a,b);
+        case 'divide':
+            return divide(a,b);
+        case 'multiply':
+            return multiply(a.b);
+        default:
+            return 'You did not choose what you would like to do'
+    }
+}
+
+alert(operate(dig1,dig2));
